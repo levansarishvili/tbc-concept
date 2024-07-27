4e638578e4908189227431b12f2cc63a22afd2ae
levani_22 <levanisarishvili322@gmail.com>
2024-07-27 15:09:24 +0400

    feat: add footer dropdown accordions functionality and modify media queries for better responsive design

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 // /////////////// Header Dropdown Component ///////////////
+
 document.addEventListener("DOMContentLoaded", () => {
   const headerNav = document.querySelector(".header__nav");
   const headerDropdownContainer = document.querySelector(
@@ -31,8 +32,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // I use Event Delegation
   headerNav.addEventListener("click", (e) => {
-    index = e.target.getAttribute("data-index");
     const target = e.target;
+    index = target.getAttribute("data-index");
 
     // Check if i clicked on correct element
     if (target.classList.contains("header__dropdown-toggle")) {
@@ -78,8 +79,71 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // /////////////// Header Hamburger Menu ///////////////
+
 const burgerMenuButton = document.querySelector(".header__burger-menu");
 
 burgerMenuButton.addEventListener("click", () => {
   burgerMenuButton.classList.toggle("active");
+});
+
+// /////////////// Footer Dropdown Menu ///////////////
+
+const footerMainContent = document.querySelector(".footer__content__inner");
+const footerDropdownToggles = document.querySelectorAll(
+  ".footer__dropdown-toggle"
+);
+const footerDropdownListWrappers = document.querySelectorAll(
+  ".footer__dropdown__list-wrapper"
+);
+const footerDropdownArrows = document.querySelectorAll(
+  ".footer__dropdown__arrow"
+);
+
+// Close all footer dropdown function
+const closeAllFooterDropdown = () => {
+  footerDropdownListWrappers.forEach((el) => {
+    el.classList.remove("open");
+  });
+  footerDropdownArrows.forEach((el) => {
+    el.classList.remove("open");
+  });
+};
+
+// I use Event Delegation to avoid multiple event listeners on each toggler
+footerMainContent.addEventListener("click", (e) => {
+  const target = e.target;
+  const index = target.getAttribute("data-index");
+
+  if (target.classList.contains("footer__dropdown-toggle")) {
+    const isOpen = footerDropdownListWrappers[index].classList.contains("open");
+
+    // Close all dropdowns at first
+    closeAllFooterDropdown();
+
+    // If the clicked toggle was not open before, open it
+    if (!isOpen) {
+      footerDropdownListWrappers[index].classList.add("open");
+      footerDropdownArrows[index].classList.add("open");
+    }
+  }
+});
+
+// /////////////// Cookies Component ///////////////
+
+const cookiesContainer = document.querySelector(".cookies-wrapper");
+const cookiesAcceptButton = document.querySelector(".cookies-button");
+
+cookiesAcceptButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  cookiesContainer.classList.add("hidden");
+});
+
+// /////////////// Button Menu Component ///////////////
+const buttonMenuTrigger = document.querySelector(".button-menu__trigger");
+const innerButtons = document.querySelector(".buttons");
+
+buttonMenuTrigger.addEventListener("click", () => {
+  innerButtons.classList.toggle("open");
+  buttonMenuTrigger.classList.toggle("open");
 });
